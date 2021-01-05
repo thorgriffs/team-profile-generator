c64927ad581a5319b8120e78f3d89c13875827e6
thorgriffs <22.kelliking@gmail.com>
2021-01-05 01:55:56 -0500

    added email-validator module, added inquirer questions and render functions

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const collectEmployeeInfo = require("./collectEmployeeInfo");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+// Collect employees with inquirer
+
+collectEmployeeInfo(renderHtml);
+
+function renderHtml(answers) {
+	
+	console.log('\nrenderHtml\n');
+	
+	var employees = [];
+
+	for (let answer of answers) {
+		
+		let ee;
+		
+		if (answer.role === 'Manager') {
+			ee = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+		}
+		else if (answer.role === 'Intern') {
+			ee = new Intern(answer.name, answer.id, answer.email, answer.school);
+		}
+		else if (answer.role === 'Engineer') {
+			ee = new Engineer(answer.name, answer.id, answer.email, answer.github);
+		}
+		
+		if (ee) {
+			employees.push(ee);
+		}
+	}
+	
+	var html = render(employees);
+	fs.writeFileSync(outputPath, html);
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
---
+++ collectEmployeeInfo.js
@@ -0,0 +1,130 @@
+const inquirer = require('inquirer');
+const validator = require('email-validator');
+
+module.exports = function(cb) {
+	askForManagerInfo(cb);
+}
+
+var validateNotBlank = function (value) {
+  var pass = !!value.trim();
+  if (pass) {
+	return true;
+  }
+
+  return 'Please enter something!';
+}
+
+var commonQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What is the employee's name?",
+    validate: validateNotBlank,
+	when: function (answers) {
+      return !answers.done;
+    }
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What is the employee's id?",
+    validate: validateNotBlank,
+	when: function (answers) {
+      return !answers.done;
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What is the employee's email address?",
+    validate: function(email) {
+		var pass = validator.validate(email);
+		if(pass) {
+            return true;
+        }
+        return 'Please enter a valid email address';
+	},
+	when: function (answers) {
+      return !answers.done;
+    }
+  }
+];
+
+var officeNumberQuestion = {
+	type: 'input',
+	name: 'officeNumber',
+	message: "What is the employee's office number?",
+	validate: validateNotBlank,
+};
+var doneQuestion = {
+    type: 'confirm',
+    name: 'done',
+    message: 'Are you done?',
+    default: false,
+};
+
+var managerQuestions = commonQuestions.concat([officeNumberQuestion, doneQuestion]);
+
+var roleQuestion = {
+	type: 'list',
+	name: 'role',
+	message: 'What role would you like to add?',
+	choices: ['Intern', 'Engineer']
+};
+var githubQuestion = {
+    type: 'input',
+    name: 'github',
+    message: "What is the employee's github username?",
+    validate: validateNotBlank,
+	when: function (answers) {
+      return !answers.done && answers.role === 'Engineer';
+    }
+  };
+var schoolQuestion = {
+    type: 'input',
+    name: 'school',
+    message: "What is the employee's school?",
+    validate: validateNotBlank,
+	when: function (answers) {
+      return !answers.done && answers.role === 'Intern';
+    }
+  };
+
+var nonManagerQuestions = [roleQuestion, ...commonQuestions, githubQuestion, schoolQuestion, doneQuestion];
+
+function askForManagerInfo(onDone) {
+	
+	console.log("Hello. Let's gather the Manager's information.\n");
+	var employees = [];
+
+	inquirer.prompt(managerQuestions).then((answers) => {
+		
+		//not asking for the initial role selection, needs to be added
+		answers.role = 'Manager';
+		
+		employees.push(answers);
+		
+		if (!answers.done) {
+			askForNonManagers(employees, onDone);
+		}
+		else {
+			onDone(employees);
+		}
+		
+	});
+}
+
+function askForNonManagers(employees, onDone) {
+	
+	inquirer.prompt(nonManagerQuestions).then(answers => {
+		
+		employees.push(answers);
+		
+		if (!answers.done) {
+			askForNonManagers(employees, onDone);
+		}
+		else {
+			onDone(employees);
+		}
+	});
+}
